50fdf9036112ab1f8bb2a8b708e6d4219dd385e8
Gui11e19 <40211527+Gui11e19@users.noreply.github.com>
2024-03-10 20:20:05 -0600

    practica 1

---
+++ p1.py
@@ -0,0 +1,7 @@
+a=input("escribe el primer nombre de la cerveceria: ")
+b=input("escribe el segundo nombre de la cerveceria: ")
+
+print("la cerveceria se llama:\n" + "\" " + a + " " + b + " \"")
+
+
+
