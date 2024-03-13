4bd97bd253dbe4b40f7da1de6192126e7a871bdc
Gui11e19 <40211527+Gui11e19@users.noreply.github.com>
2024-03-12 21:59:04 -0600

    tuples

--- practicas/14. diccionario.py
+++ practicas/14. diccionario.py
@@ -20,8 +20,42 @@ cliente = {'nombre':'Guille','apellido':'Salazar','peso':'200lbs','altura':'174c
 consulta = (cliente['apellido'])
 print(consulta)
 
+EJEMPLO 3
+dic = {'c1':55,'c2':[10,20,30],'c3':{'s1':100,'s2':200}}
+#imprimir posicion especifica de una lista #print(dic['c2'][0])
 
+#imprimir posicion especifica de una lista #print(dic['c3']['s2'])
+
+EJEMPLO 4
+imprimir en mayuscula la E de la lista en c2
+
+dic = {'c1':['a','b','c'],'c2':['d','e','f']}
+print(dic['c2'][1].upper())
+
+EJEMPLO 5
+agregar un elemento
+dic = {1:'a',2:'b'}
+dic[3] = 'c'
+
+modificar un elemento
+dic = {1:'a',2:'b'}
+dic[1] = 'hola'
+print(dic)
+
+imprimir llaves
+print(dic.keys())
+
+imprimir valores
+print(dic.values())
+
+imprimir todos los elementos
+print(dic.items())
 """
+dic = {1:'a',2:'b'}
+print(dic.items())
+
+
+
 
 
 
